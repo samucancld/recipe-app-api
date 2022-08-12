7fa1ae3a20a666aaa88ebdf5faa34974ffa6b44f
samucancld <samuel5848@gmail.com>
2022-08-11 22:10:21 -0300

    Custom user model y tdd con email normalizado

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,42 @@
-from django.db import models # noqa
+"""
+Databse models.
+"""
 
-# Create your models here.
+
+from django.db import models # noqa
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
+
+
+class UserManager(BaseUserManager):
+    """ Manager for users. """
+
+    def create_user(self, email, password=None, **extra_fields):
+        user = self.model(
+            email=self.normalize_email(email),
+            **extra_fields
+        )
+        user.set_password(password)
+        # _db es por si cambias de db
+        user.save(using=self._db)
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """ User in the system. """
+    email = models.EmailField(
+        max_length = 255,
+        unique = True,
+    )
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+
