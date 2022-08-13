55fd3bbf3008ba82e0ef577d108d5d878c385472
samucancld <samuel5848@gmail.com>
2022-08-13 00:43:23 -0300

    Admin pages tested

--- app/core/models.py
+++ app/core/models.py
@@ -17,10 +17,19 @@ class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
         user = self.model(
             email=self.normalize_email(email),
-            **extra_fields
+            **extra_fields,
         )
         user.set_password(password)
         # _db es por si cambias de db
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        """Creates and saves a new super user"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
         user.save(using=self._db)
 
         return user
